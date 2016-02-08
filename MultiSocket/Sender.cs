5f39dbd90caa7e12ab8605e01a92fdaee94e7624
EnchMan <sammyextra@gmail.com>
2016-02-08 13:39:42 +0100

    Start Project

---
+++ MultiSocket/Sender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace MultiSocket
+{
+    public class Sender
+    {
+        public string Address { get; set; }
+        public int Port { get; set; }
+
+        public byte[] Frames;
+
+        
+    }
+}
